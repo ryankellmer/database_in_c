2e63555d53e10f5733d293dfb1a8c926bcb3a8b9
ryankellmer <ryankellmer@gmail.com>
2025-11-14 21:54:38 -0800

    Added create_database and open_database logic

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[]) {
   char *filepath = NULL;
   bool newfile = false;
   char c;
+  int dbfd = -1;
 
   while (-1 != (c = getopt(argc, argv, "nf:"))) {
     switch (c) {
@@ -39,6 +40,20 @@ int main(int argc, char *argv[]) {
     printf("Filepath is a required field\n");
     print_usage(argv);
     return 0;
+  }
+
+  if (newfile) {
+    dbfd = create_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+      printf("Unable to create database file\n");
+      return -1;
+    }
+  } else {
+    dbfd = open_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+      printf("Unable to open database file\n");
+      return -1;
+    }
   }
 
   printf("Newfile: %d\n", newfile);
