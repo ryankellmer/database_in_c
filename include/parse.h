8302910e391ae20f21d147d79633a4257cc444c4
Ryan Kellmer <61802954+ryankellmer@users.noreply.github.com>
2025-11-18 19:55:23 -0800

    Added ability to add employees to db (#3)
    * Added create_db_header and validate_db_header
    
    * Fixed header pointer comparison and check fstat for error
    
    * Added the ability to add employees to db.

--- include/parse.h
+++ include/parse.h
@@ -18,6 +18,11 @@ struct employee_t {
 
 int create_db_header(int fd, struct dbheader_t **headerOut);
 int validate_db_header(int fd, struct dbheader_t **headerOut);
-void output_file(int fd, struct dbheader_t *dbhdr);
+int read_employees(int fd, struct dbheader_t *dbhdr,
+                   struct employee_t **employeesOut);
+int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees,
+                 char *addstring);
+void output_file(int fd, struct dbheader_t *dbhdr,
+                 struct employee_t *employees);
 
 #endif
