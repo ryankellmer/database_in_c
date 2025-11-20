113ecf360f5b58e05619bc439914f351899c10b6
ryankellmer <ryankellmer@gmail.com>
2025-11-19 21:00:36 -0800

    "Updated the format for print_employees"

--- src/parse.c
+++ src/parse.c
@@ -13,8 +13,10 @@
 int list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
   int employeeCount = dbhdr->count;
   for (int i = 0; i < employeeCount; i++) {
-    printf("%s %s %d hours\n", employees[i].name, employees[i].address,
-           employees[i].hours);
+    printf("Employee %d\n", i);
+    printf("\tName: %s\n", employees[i].name);
+    printf("\tAddress: %s\n", employees[i].address);
+    printf("\tHours: %d\n", employees[i].hours);
   }
   return STATUS_SUCCESS;
 }
